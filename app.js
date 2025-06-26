a1801b2c5556508062a86666a4b0bfcd814c8070
APD007 <prasadayush036@gmail.com>
2025-06-27 05:01:21 +0530

    UPDATED

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import ejsMate from "ejs-mate";
 import { fileURLToPath } from "url";
 import mongoose from "mongoose";
 import { Listing } from "./models/listing.js";
@@ -12,6 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.engine("ejs", ejsMate);
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public/js")));
@@ -35,20 +37,26 @@ async function connectDB() {
   }
 }
 
-//Index Route
 app.get("/", async (req, res) => {
   res.render("working");
 });
 
+//INDEX ROUTE
 app.get("/listings", async (req, res) => {
     try {
         const listings = await Listing.find({});
-        res.render("listings/index", { listings }); // Assuming you have views/listings/index.ejs
+        res.render("listings/index", { listings }); 
     } catch (err) {
         console.error("Error fetching listings:", err);
         res.status(500).send("Something went wrong!");
     }
 });
+
+//NEW ROUTE
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new");
+});
+
 
 app.get("/listings/:id", async (req, res) => {
     try {
@@ -63,6 +71,56 @@ app.get("/listings/:id", async (req, res) => {
         res.status(500).send("Something went wrong!");
     }
 });
+
+app.post("/listings", async (req, res) => {
+  try {
+    const listing = new Listing(req.body.listing); 
+    await listing.save();
+    res.redirect("/listings");
+  } catch (err) {
+    console.error("Failed to create listing:", err);
+    res.status(500).send("Error creating listing.");
+  }
+});
+
+
+//EDIT ROUTE
+app.get("/listings/:id/edit", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit", { listing });
+  } catch (err) {
+    console.error("Error loading edit form:", err);
+    res.status(500).send("Error loading edit form");
+  }
+});
+
+app.put("/listings/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedListing = await Listing.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.redirect(`/listings/${updatedListing._id}`);
+  } catch (err) {
+    console.error("Error updating listing:", err);
+    res.status(500).send("Update failed");
+  }
+});
+
+// DELETE route
+app.delete("/listings/:id", async (req, res) => {
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.redirect("/listings");
+  } catch (err) {
+    console.error("❌ Error deleting listing:", err);
+    res.status(500).send("Something went wrong!");
+  }
+});
+
 
 
 connectDB().then(() => {
